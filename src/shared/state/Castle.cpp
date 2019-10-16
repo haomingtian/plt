041c0977e28c9faf5f0080752570adda32647f18
Marlyatou Diallo <marlyatou.diallo@ensea.fr>
2019-10-16 17:11:06 +0200

    fichiers png ajoutés à ressources

---
+++ src/shared/state/Castle.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include "Castle.h"
+using namespace std;
+
+namespace state {
+Castle ::Castle(CastleTypeId id, int n_x, int n_y, int n_codetuile){
+	type_id=id;
+	if(id==FIRSTLEVEL){
+		name="FIRSTLEVEL";		
+	}
+	else if(id==SECONDLEVEL){
+		name="SECONDLEVEL";
+	}
+	else if(id==THIRDLEVEL){
+		name="THIRDLEVEL";
+	}
+	else if(id==FINALLEVEL){
+		name="THIRDLEVEL";
+	}
+	else if(id==DEMOLISHED){
+		name="DEMOLISHED";
+	}
+	
+}
+CastleTypeId  Castle::getType (){
+	return type_id;
+}
+	
+}
